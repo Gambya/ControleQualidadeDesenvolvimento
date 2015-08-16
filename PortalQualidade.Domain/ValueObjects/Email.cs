9884be813cbd2ec7167aa44191c053b1ba1e4895
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 17:23:37 -0300

    ValueObject de email

--- PortalQualidade.Domain/ValueObjects/Email.cs
+++ PortalQualidade.Domain/ValueObjects/Email.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using PortalQualidade.Domain.Resources;
+using PortalQualidade.Helpers;
+using PortalQualidade.Helpers.Resource;
 
 namespace PortalQualidade.Domain.ValueObjects
 {
@@ -15,7 +19,15 @@ namespace PortalQualidade.Domain.ValueObjects
 
         public Email(string email)
         {
+            Guard.ForNullOrEmptyDefaultMessage(email, "Email");
+            Guard.StringLength("Email", email, EmailMaxLength);
+            if (!IsEmailValid(email))
+                throw new Exception(Errors.InvalidEmail);
             this.EMail = email;
+        }
+        public static bool IsEmailValid(string email)
+        {       
+            return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
         }
     }
 }
