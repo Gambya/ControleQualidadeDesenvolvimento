b2e7843ae18fe0a8fa0e21333e6fb73e2bf5d541
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 17:24:02 -0300

    Teste de ValueObject Email

--- PortalQualidade.Domain.Tests/ValueObjects/EmailTests.cs
+++ PortalQualidade.Domain.Tests/ValueObjects/EmailTests.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortalQualidade.Domain.ValueObjects;
 
 namespace PortalQualidade.Domain.Tests.ValueObjects
 {
+    [TestClass]
     public class EmailTests
     {
+        [TestMethod]
+        [ExpectedException(typeof (Exception))]
+        public void EmailEmBranco()
+        {
+            var email = new Email("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmailNull()
+        {
+            var email = new Email(null);
+        }
+
+        [TestMethod]
+        public void EmailValido()
+        {
+            var endereco = "robertolopesramos@gmail.com";
+            var email = new Email(endereco);
+            Assert.AreEqual(endereco,email.EMail);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmailInvalido()
+        {
+            var email = new Email("sdsdsdsdsdsdsfsfs");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EmailErroMaxLeght()
+        {
+            var endereco = "robertolopesramos@gmail.com";
+            while (endereco.Length < Email.EmailMaxLength+1)
+            {
+                endereco = endereco + "robertolopesramos@gmail.com";
+            }
+            new Email(endereco);
+        }
+
     }
 }
