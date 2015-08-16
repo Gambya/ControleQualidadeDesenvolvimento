9fd318d89d19ec6e6464dd6137c55086b490ff66
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 19:23:01 -0300

    Adicionado testes para Password.

---
+++ PortalQualidade.Domain.Tests/ValueObjects/PasswordTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortalQualidade.Domain.ValueObjects;
+using PortalQualidade.Helpers;
+
+namespace PortalQualidade.Domain.Tests.ValueObjects
+{
+    [TestClass]
+    public class PasswordTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof (Exception))]
+        public void PasswordNullOrEmpty()
+        {
+            Guard.ForNullOrEmpty("","Password Inválido.");
+        }
+    }
+}
