7c02c5b03d13e341d0c92557cb68824919ce4211
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 19:22:07 -0300

    ValueObject de password adicionado.

---
+++ PortalQualidade.Domain/ValueObjects/Password.cs
@@ -0,0 +1,23 @@
+﻿using PortalQualidade.Domain.Resources;
+using PortalQualidade.Helpers;
+
+namespace PortalQualidade.Domain.ValueObjects
+{
+    public class Password
+    {
+        public string Senha { get; private set; }
+
+        protected Password(){}
+
+        public Password(string senha)
+        {
+            SetPassword(senha);
+        }
+
+        private void SetPassword(string senha)
+        {
+            Guard.ForNullOrEmpty(senha, Errors.InvalidPassword);
+            Senha = Guard.Encrypt(senha);
+        }
+    }
+}
