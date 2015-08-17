18019352edfa333fbabf2332bad8d89ae912aaf2
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 23:46:42 -0300

    Add ValueObject Password;

--- PortalQualidade.Domain/ValueObjects/Password.cs
+++ PortalQualidade.Domain/ValueObjects/Password.cs
@@ -5,18 +5,23 @@ namespace PortalQualidade.Domain.ValueObjects
 {
     public class Password
     {
+        public const int PasswordMaxLenght = 20;
+        public const int PasswordMinLenght = 6;
         public string Senha { get; private set; }
 
         protected Password(){}
 
-        public Password(string senha)
+        public Password(string senha, string confirmaSenha)
         {
-            SetPassword(senha);
+            SetPassword(senha, confirmaSenha);
         }
 
-        private void SetPassword(string senha)
+        private void SetPassword(string senha, string confirmaPassword)
         {
             Guard.ForNullOrEmpty(senha, Errors.InvalidPassword);
+            Guard.ForNullOrEmpty(confirmaPassword, Errors.InvalidConfirmaPasswordNullOrEmpty);
+            Guard.AreEqual(senha, confirmaPassword, Errors.InvalidConfirmaPassword);
+            Guard.StringLength("Password", senha, PasswordMinLenght, PasswordMaxLenght);
             Senha = Guard.Encrypt(senha);
         }
     }
