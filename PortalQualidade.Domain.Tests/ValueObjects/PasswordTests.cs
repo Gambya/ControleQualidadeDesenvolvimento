15b23ec87202d0d5f0a09f7cba05cb271df101ff
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-16 23:47:21 -0300

    Add Unite tests do ValueObject Password;

--- PortalQualidade.Domain.Tests/ValueObjects/PasswordTests.cs
+++ PortalQualidade.Domain.Tests/ValueObjects/PasswordTests.cs
@@ -8,11 +8,29 @@ namespace PortalQualidade.Domain.Tests.ValueObjects
     [TestClass]
     public class PasswordTests
     {
+        public Password Password { get; set; }
+
+        public PasswordTests()
+        {
+            Password = new Password("123456", "123456");
+        }
         [TestMethod]
         [ExpectedException(typeof (Exception))]
         public void PasswordNullOrEmpty()
         {
             Guard.ForNullOrEmpty("","Password Inválido.");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PasswordMinMaxLenght()
+        {
+            if (Password.Senha.Length < 6 || Password.Senha.Length > 20)
+                throw new Exception("Password não pode ser menor que 6 caracters nem maior que 20 caracteres.");
+        }
+        [TestMethod]
+        public void PasswordEIgualAoConfirmaPassword()
+        {
+            Assert.AreEqual(Guard.Encrypt("123456"),Password.Senha);
         }
     }
 }
