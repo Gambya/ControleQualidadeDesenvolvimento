9c01e03b8b0a9d3d627bee1ac4bd499a5b792fa8
Roberto Lopes Ramos <robertolopesramos@gmail.com>
2015-08-18 00:31:55 -0300

    Add validações a entidade

--- PortalQualidade.Domain/Entities/Usuario.cs
+++ PortalQualidade.Domain/Entities/Usuario.cs
@@ -1,27 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PortalQualidade.Domain.Resources;
 using PortalQualidade.Domain.ValueObjects;
+using PortalQualidade.Helpers;
 
 namespace PortalQualidade.Domain.Entities
 {
     public class Usuario:EntityBase
     {
+        public const int NomeMaxLenght = 100;
         public string Nome { get; private set; }
-        public Email _Email { get; private set; }
+        public const int LoginMaxLenght = 50;
+        public string Login { get; private set; }
+        public Password Password { get; private set; }
+        public Guid TokenAlteracao { get; private set; }
+        public Email EMail { get; private set; }
+        public const int DepartamentoMaxLenght = 60;
         public string Departamento { get; private set; }
         public bool Nivel { get; private set; }
         public bool Flag { get; private set; }
 
+        //Contrutor EntityFramework
         protected Usuario(){}
 
-        public Usuario(string nome, Email email, string departamento)
+        public Usuario(string nome, string email, string login, string password, string confirmaPassword, string departamento, bool nivel = false, bool flag = false)
         {
-            this.Nome = nome;
-            this._Email = email;
-            this.Departamento = departamento;
+            SetNome(nome);
+            SetEmail(email);
+            SetLogin(login);
+            SetPassword(password,confirmaPassword);
+            SetDepartamento(departamento);
+            Nivel = nivel;
+            Flag = flag;
+        }
+
+        public void SetNome(string nome)
+        {
+            Guard.ForNullOrEmpty(nome,Errors.InvalidNome);
+            Nome = nome;
+        }
+
+        public void SetLogin(string login)
+        {
+            Guard.ForNullOrEmpty(login,Errors.InvalidLogin);
+            Login = login;
+        }
+
+        private void SetPassword(string password, string confirmaPassword)
+        {
+            Password = new Password(password, confirmaPassword);
+        }
+
+        public void SetEmail(string email)
+        {
+            EMail = new Email(email);
+        }
+
+        public void SetDepartamento(string departamento)
+        {
+            Departamento = departamento;
         }
     }
 }
